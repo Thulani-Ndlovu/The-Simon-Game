d40b35060522cb87a7ddd0e5a9e10800f33ca656
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-05-27 19:01:49 +0200

    Animations and Sounds

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
-let randomChosenColour = buttonColours[randomNumber];
 let gamePattern = [];
-gamePattern.push(randomChosenColour);
-
-
-
-
 
 
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
-    return (randomNumber);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/"+randomChosenColour +".mp3");
+    audio.play();
 }
+
